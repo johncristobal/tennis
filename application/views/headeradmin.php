dbd537fc8bc56794626cfe7c15d488087ddde0f9
John_Vera <johnalexiscristobaljimenez6@gmail.com>
2017-11-07 22:55:56 -0600

    header admin

---
+++ application/views/headeradmin.php
@@ -0,0 +1,36 @@
+   		<body>
+		<header>
+           <div class="container">
+            <div class="row">
+        	<div class="col-md-4 col-xs-3"><h1 id="logo"><a href="<?php echo base_url();?>" title="Planar - Personal Trainer">Tenis - Sportway</a></h1></div>
+            <nav class="col-md-8 col-xs-9">
+            	<ul id="main-nav" class="sf-menu" >
+                <!--li><a href="<?php echo base_url();?>Player/perfil">Jugadores</a></li-->
+                    <!--li><a href="profile.html"></a></li-->
+                				
+                <li><a href="<?php echo base_url();?>admin/inicio">Torneos</a>
+                <!--ul>
+                    <li><a href="<?php echo base_url();?>torneos/calendario">Calendario</a></li>
+                        <li><a href="calories-burned-heart-rate.html">Partidos</a></li>
+                        <li><a href="calories-burned-calculator.html">Resultados</a></li>
+                  	</ul-->
+                </li>
+                <li><a href="<?php echo base_url();?>admin/cerrar">Cerrar sesión</a>
+                </li>				
+               <!-- <li><a href="#">Pages</a>
+                	<ul>
+                    	<li><a href="pricing.html">Pricing</a></li>
+                        <li><a href="blog.html">Blog</a></li>
+                        <li><a href="blog_post.html">Single post</a></li>
+                        <li><a href="elements.html">Elements</a></li>
+                        <li><a href="img-galleries.html">Images and galleries</a></li>
+                        <li><a href="media.html">Media</a></li>
+                  	</ul>
+                </li>
+                <li><a href="contact.html">Contact</a></li>
+                <li><a href="#">Purchase</a></li>-->
+                </ul>
+            </nav><!-- End Nav -->
+         </div>
+        </div><!-- End container -->
+        </header> <!-- End header --> 
